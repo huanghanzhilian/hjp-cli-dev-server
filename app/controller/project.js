dd550fbf19676cca66866491101edc018a07d2fb
huangjipeng <1319639755@qq.com>
2022-03-27 13:35:30 +0800

    chore: clear log

--- app/controller/project.js
+++ app/controller/project.js
@@ -7,7 +7,7 @@ class ProjectController extends Controller {
   // 获取项目/组件的代码模板
   async getTemplate() {
     const { ctx } = this;
-    let db = await mongo()
+    // let db = await mongo()
     const data = await mongo().query('project')
     // console.log(data)
     ctx.body = data
